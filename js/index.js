e1836fe5fa096a14128b522eebf20dcbbf79df08
Evan Eckels <eveckels@gmail.com>
2018-04-03 21:44:17 -0500

    added all

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(document).ready( function() {
+  var clientHeight = $(window).height();
+  $('#parallax').css('height', clientHeight);
+  $('.backrow').css('height', clientHeight);
+  $('.middlerow').css('height', clientHeight);
+  $('.frontrow').css('height', clientHeight);
+  /*$('.bottom').css('margin-top', clientHeight);*/
+  console.log(clientHeight);
+
+$(window).resize(function() {
+    clientHeight = $(window).height();
+    $('#parallax').css('height', clientHeight);
+    $('.backrow').css('height', clientHeight);
+    $('.middlerow').css('height', clientHeight);
+    $('.frontrow').css('height', clientHeight);
+    /*$('.bottom').css('margin-top', clientHeight);*/
+    console.log(clientHeight);
+  });
+
+});
