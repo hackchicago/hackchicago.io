8058980222866caa9ef3dab9e1e5a0944dde5a8f
JSneak <jsneak@Seans-MacBook-Pro.local>
2018-04-16 13:04:47 -0500

    Add email validation

--- js/collection.js
+++ js/collection.js
@@ -10,12 +10,32 @@ var database = firebase.database();
 
 function pushData(){
   var data = document.getElementById("emails").value;
-  var dataRef = database.ref('/').push();//Generates a new child location with a randomly generated id.
-  dataRef.set({ value: data })
-  .then(function() {
-    success();
-  })
-  .catch(function(error) {
-    console.log('Synchronization failed');
-  });
+  if(validate(data)) {
+    var dataRef = database.ref('/').push();//Generates a new child location with a randomly generated id.
+    dataRef.set({ value: data })
+    .then(function() {
+      success();
+    })
+    .catch(function(error) {
+      console.log('Synchronization failed');
+    });
+  }else{
+    failure();
+  }
+}
+
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function validate(email) {
+  console.log('test')
+  if (validateEmail(email)) {
+    console.log("working");
+    return true;
+  } else {
+    console.log("wrong")
+    return false;
+  }
 }
