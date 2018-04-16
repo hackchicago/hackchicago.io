43702289d57320cc9638e2e654cf31d3f03e5458
JSneak <jsneak@Seans-MacBook-Pro.local>
2018-04-16 09:01:13 -0500

    Update Variable name

--- js/collection.js
+++ js/collection.js
@@ -9,7 +9,7 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 function pushData(){
-  var data = document.getElementById("email").value;
+  var data = document.getElementById("emails").value;
   var dataRef = database.ref('/').push();//Generates a new child location with a randomly generated id.
   dataRef.set({
     value: data
