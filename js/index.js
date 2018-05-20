3148a53b1518adb68e1d80db1548e73fc57ecb0b
Zane <contact@zane.sh>
2018-05-20 12:00:23 -0700

    Add copying functionality Gracefully degrades using Clipboard API & document.execCommand()

--- js/index.js
+++ js/index.js
@@ -161,10 +161,29 @@ $('.ap-reset').on('click', function() {
 
 function setAPLink(n) {
   $('.ap-form').hide();
-  $('.ap-result').html("<p>Your unique link: <input class=\"ap-link\" value=\"https://hackchicago.io/?ref=" + n.replace(" ", "+") + "\"><button class=\"copy-button\"><svg class=\"copy-icon\" viewBox=\"0 0 14 16\" version=\"1.1\" width=\"14\" height=\"16\" aria-hidden=\"true\"><path fill-rule=\"evenodd\" d=\"M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z\"></path></svg></button></p>");
+  $('.ap-result').html("<p>Your unique link: <input class=\"ap-link\" value=\"https://hackchicago.io/?ref=" + n.replace(" ", "+") + "\"><button id=\"copy_button\" class=\"copy-button\"><svg class=\"copy-icon\" viewBox=\"0 0 14 16\" version=\"1.1\" width=\"14\" height=\"16\" aria-hidden=\"true\"><path fill-rule=\"evenodd\" d=\"M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z\"></path></svg></button></p>");
+  $(".copy-button").on("click", function () {
+    if (!navigator.clipboard) {
+      $(".ap-link").focus();
+      $(".ap-link").select();
+      try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'Copied!' : 'Press CTRL + C or CMD + C to copy';
+        $(".copy-button-result").text(msg);
+      } catch (err) {
+        $(".copy-button-result").text('Press CTRL + C or CMD + C to copy');
+      }
+    } else {
+      navigator.clipboard.writeText($('.ap-link').val()).then(function() {
+        $(".copy-button-result").text('Copied!');
+      }, function(err) {
+        $(".copy-button-result").text('Press CTRL + C or CMD + C to copy');
+      });
+    }
+  });
   $(".ap-link").on("click", function () {
     $(this).select();
- });
+  });
   $('.ap-link').attr('size', $('.ap-link').val().length);
   $('.ap-name-box').text(n);
   $('.ap-reset-bar').show();
