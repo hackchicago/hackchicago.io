1c53a6b1e94bcbbb3e892e39fff7ae5564455a8b
Zane <contact@zane.sh>
2018-06-02 22:40:30 -0700

    Add referral logic

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+$(document).ready(function() {
+  // check URL for referral code
+  let urlRef = getParam('ref');
+  if (urlRef != null && urlRef !== "" && urlRef !== "null" && urlRef != undefined) Cookies.set('ref', urlRef, { expires: 180 });
+}
+
 // scroll up to top of page on <a href="#top">
 $('a[href="#top"]').click(function() {
   $("html, body").animate({ scrollTop: 0 }, "slow");
