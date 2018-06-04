cc678d95e1a18659e3c43974164793c316ab2cc2
Zane <contact@zane.sh>
2018-06-03 22:42:19 -0700

    Set referral cookie by URL

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  // check URL for referral code
+  let urlRef = getParam('ref');
+  if (urlRef != null && urlRef !== "" && urlRef !== "null" && urlRef != undefined) Cookies.set('ref', urlRef, { expires: 180 });
+}
+
+// get URL params
+function getParam(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
