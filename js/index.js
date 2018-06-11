7d42d0ae7f99a71646c1bfbd05632dc0d70d9740
Zane <contact@zane.sh>
2018-06-11 14:40:40 -0700

    Handle signed up users on homepage

--- js/index.js
+++ js/index.js
@@ -5,6 +5,21 @@ let urlRef = getParam('ref');
 if (urlRef != null && urlRef !== "" && urlRef !== "null" && urlRef != undefined)
   Cookies.set('ref', urlRef, { expires: 180 });
 
+// check URL for success flag
+if (getParam('success') == 'true') Cookies.set('hasSignedUp', 'true', { expires: 180 });
+
+// handle signed up attendee or referral
+if(Cookies.get('hasSignedUp') == 'true') {
+  $('#apply-button').html('<b>Your application has been submitted!</b>').css('color', '#5299d3');
+} else {
+  var r = Cookies.get('ref');
+  if (r != "" && r != null && r != "null" && r != undefined) {
+    $(".apply-link").attr("href", "/apply?ref=" + r);
+    $("#ref-name").text(r);
+    $("#ref").addClass("show");
+  }
+}
+
 // get URL params
 function getParam(name, url) {
   if (!url) url = window.location.href;
@@ -15,13 +30,3 @@ function getParam(name, url) {
   if (!results[2]) return '';
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
-
-$(function() {
-    var r = Cookies.get('ref');
-    if (r != "" && r != null && r != "null" && r != undefined) {
-      $(".apply-link").attr("href", "/apply?ref=" + r);
-      $("#ref-name").text(r);
-
-      $("#ref").addClass("show");
-    }
-});
