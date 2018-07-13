65840ab9eb79a6e46d1cc0df718092bf6545481d
Zane <github@zane.sh>
2018-07-12 23:17:43 -0700

    Redirect old application requests w/ warning

--- js/index.js
+++ js/index.js
@@ -2,4 +2,19 @@ AOS.init({});
 
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
+
+  if (getParam('s') || getParam('ref')) {
+    $('#ref').addClass('success');
+  }
 })
+
+// get parameter
+function getParam(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
